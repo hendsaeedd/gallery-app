13b941f6d180f5ef0a694dcfa5876bdba25d7be8
hendsaeedd <hendsaied135@gmail.com>
2023-11-11 23:52:59 +0200

    add static path for uploads

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'assets/uploads')));//for uploaded images
 
 app.use('/', indexRouter);
 
